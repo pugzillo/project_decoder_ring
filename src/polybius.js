1a949c5454fe553c2ec77ddd7419a24f165bbd71
Linh <lcahu91@gmail.com>
2021-06-26 16:31:20 -0400

    added encoding for polybius function

--- src/polybius.js
+++ src/polybius.js
@@ -4,10 +4,44 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
-
   function polybius(input, encode = true) {
-    // your solution code here
+    if (!input.replace(/\s+/g, "").length % 2 === 0 && !encode) return false; // when number of characters, excluding spaces, is not even
+
+    // Polybius Square
+    let square = [
+      "abcde".split(""),
+      "fghijk".split(""),
+      "lmnop".split(""),
+      "qrstu".split(""),
+      "vwxyz".split(""),
+    ];
+    square = square.map((row) =>
+      row[0] === "f"
+        ? [["f"], ["g"], ["h"], ["i", "j"], ["k"]] // both i and j can be converted to 42
+        : row.map((letter) => [letter])
+    );
+
+    const inputArray = input.toLowerCase().split("");
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+    const translatedArray = inputArray.map((char) => {
+      if (!alphabet.includes(char)) return char; // return non-alphabetic characters
+      let result;
+      square.forEach((row, rowIdx) => {
+        row.forEach((col, colIdx) => {
+          if (col.find((letter) => letter === char)) {
+            result = `${colIdx + 1}${rowIdx + 1}`;
+            // when decoding, 42 represents both i and j
+            if (result === "42" && !encode) {
+              result = "(i/j)";
+            }
+          }
+        });
+      });
+      return result;
+    });
+
+    return translatedArray.join("");
   }
 
   return {
@@ -16,3 +50,5 @@ const polybiusModule = (function () {
 })();
 
 module.exports = { polybius: polybiusModule.polybius };
+
+console.log(polybiusModule.polybius("thinkful"));
