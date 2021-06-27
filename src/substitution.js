4f79290527de633d4398efc3075a592c04bbb69f
Linh <lcahu91@gmail.com>
2021-06-27 13:58:08 -0400

    added encoding

--- src/substitution.js
+++ src/substitution.js
@@ -4,15 +4,47 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+
+  // Unique characters in a string
+  function findUnique(str) {
+    str = str.split("");
+    str = new Set(str);
+    return [...str].join("");
+  }
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if(alphabet === undefined || findUnique(alphabet).length !== 26 ) return false; // returns false if no alphabet parameter is included or if invalid
+  
+    // Creating cipher object
+    const keys = alphabet.split("");
+    const values = "abcdefghijklmnopqrstuvwxyz".split("");
+    let cipher = {};
+    keys.forEach((key, index) => cipher[key] = values[index]);
+
+    const inputArray = input.toLowerCase().split("");
+    let result = [];
+
+    if(encode) {
+      result = inputArray.map((char) => {
+        for (let key of Object.keys(cipher)) {
+          if (char === cipher[key]) {
+            return key;
+          } else if (char === " ") {
+            return char;
+          }
+        }
+      })
+    }
+
+    return result.join("");
+
   }
 
   return {
     substitution,
   };
 })();
+
+console.log(substitutionModule.substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev"));
 
 module.exports = { substitution: substitutionModule.substitution };
