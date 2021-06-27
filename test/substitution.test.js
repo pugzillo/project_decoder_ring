3a96dd8edf45a34874ba08518876423cb1cf6447
Linh <lcahu91@gmail.com>
2021-06-27 10:28:44 -0400

    added initial tests from qualified examples

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,57 @@
-// Write your tests here!
+const substitutionModule = require("../src/substition");
+const substitution = substitionModule.substitution; 
+const expect = require("chai").expect;
+
+describe("Error handling", () => {
+    it("returns a false when the alphabet parameter is below 26 characters", () => {
+        const expected = false; 
+        const actual = substitution("thinkful", "short");
+        expect(actual).to.equal(expected);
+    });
+
+    it("returns a false when the alphabet parameter is above 26 characters", () => {
+        const expected = false; 
+        const actual = substitution("thinkful", "abcabcabcabcabcabcabcabcyz"); 
+        expect(actual).to.equal(expected);
+    });
+
+    it("returns a false when the alphabet parameter includes redundant characters", () => {
+        const expected = false; 
+        const actual = substitution("thinkful", "$$$e&zrdxtfcygvuhbijnokmpl"); 
+        expect(actual).to.equal(expected);
+    });
+})
+
+describe("Encoding a message", () => {
+    it("returns a encoding string when input does not include spaces", () => {
+        const expected = 'jrufscpw';
+        const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.equal(expected); 
+    });
+
+    it("returns a encoding string when input includes spaces", () => {
+        const expected = 'elp xhm xf mbymwwmfj dne';
+        const actual = substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.equal(expected); 
+    });
+
+    it("returns a encoding string when input includes special characters", () => {
+        const expected = 'elp xhm xf mbymwwmfj dne';
+        const actual = substitution("message", "$wae&zrdxtfcygvuhbijnokmpl");
+        expect(actual).to.equal(expected); 
+    });    
+})
+
+describe("Decoded a message", () => {
+    it("returns a decoded message when input does not include spaces", () => {
+        const expected = 'thinkful';
+        const actual = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false);
+        expect(actual).to.equal(expected); 
+    });
+
+    it("returns a decoded message when input includes special characters", () => {
+        const expected = "message"
+        const actual = substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
+        expect(actual).to.equal(expected); 
+    });
+})
