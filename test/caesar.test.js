49c57f6be590ead3008ae1b4a418288b84f61f6f
Linh <lcahu91@gmail.com>
2021-06-28 11:02:46 -0400

    organized tests into multiple describe blocks

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -3,7 +3,7 @@ const caesarModule = require("../src/caesar");
 const expect = require("chai").expect;
 const caesar = caesarModule.caesar;
 
-describe("caesar", () => {
+describe("Decoding", () => {
   it("should return an encoded string with a positive shift", () => {
     const expected = "wklqnixo";
     const actual = caesar("thinkful", 3);
@@ -16,15 +16,17 @@ describe("caesar", () => {
     expect(actual).to.equal(expected);
   });
 
-  it("should return a decoded string when encode is set to false", () => {
-    const expected = "thinkful";
-    const actual = caesar("wklqnixo", 3, false);
-    expect(actual).to.equal(expected);
-  });
-
   it("should return an encoded string with spaces", () => {
     const expected = "bpqa qa i amkzmb umaaiom!";
     const actual = caesar("This is a secret message!", 8);
+    expect(actual).to.equal(expected);
+  });
+});
+
+describe("Encoding", () => {
+  it("should return a decoded string when encode is set to false", () => {
+    const expected = "thinkful";
+    const actual = caesar("wklqnixo", 3, false);
     expect(actual).to.equal(expected);
   });
 
@@ -33,7 +35,9 @@ describe("caesar", () => {
     const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
     expect(actual).to.equal(expected);
   });
+});
 
+describe("Caesar: Error Handling", () => {
   it("should return false if shift is not entered", () => {
     const expected = false;
     const actual = caesar("thinkful");
